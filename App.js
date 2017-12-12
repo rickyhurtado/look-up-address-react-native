ef23e66748c8d121d67545bc3caf2778574e4cd4
Ricky Hurtado <odatruh.ykcir@gmail.com>
2017-12-12 22:30:00 +1300

    Add lookup address button

--- App.js
+++ App.js
@@ -1,21 +1,57 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, StyleSheet, View, Alert } from 'react-native';
+import { Button } from 'react-native-elements';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>React Native initial files.</Text>
+        <StatusBar hidden={true} />
+        <View style={styles.contentContainer}>
+        </View>
+        <View style={styles.lookupButton}>
+          <Button
+            large
+            title="LOOKUP CURRENT ADDRESS"
+            backgroundColor="#428bca"
+            onPress={lookupCurrentAddress}
+          />
+        </View>
       </View>
     );
   }
 }
 
+const lookupCurrentAddress = () => {
+  lookingUpAddressMessage();
+}
+
+const lookingUpAddressMessage = () => {
+  errorMessage();
+}
+
+const errorMessage = () => {
+  Alert.alert(
+    'Ooops!',
+    'Sorry, something went wrong. Please try in a few minutes or restart the application.',
+    [{text: 'Close'}],
+    { cancelable: false }
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
+  contentContainer: {
+    flex: 1,
+  },
+  lookupButton: {
+    height: 70
+  }
 });
