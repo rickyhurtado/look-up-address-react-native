435eec44c1043390ce6fb6116c78a10e804f12de
Ricky Hurtado <odatruh.ykcir@gmail.com>
2017-12-13 00:25:18 +1300

    Display Google map with current location

--- App.js
+++ App.js
@@ -2,17 +2,68 @@ import React from 'react';
 import { StatusBar, StyleSheet, View, Text, Alert } from 'react-native';
 import { Button } from 'react-native-elements';
 import Modal from 'react-native-modal';
+import MapView from 'react-native-maps';
 
 export default class App extends React.Component {
   state = {
-    showModal: false
+    showModal: false,
+    mapInitialised: false,
+    mapRegion: null,
+    latitude: null,
+    longitude: null
   };
+
+  componentDidMount() {
+    this.watchID = navigator.geolocation.watchPosition((position) => {
+      let region = {
+        latitude: position.coords.latitude,
+        longitude: position.coords.longitude,
+        latitudeDelta: 0.00922*1.5,
+        longitudeDelta: 0.00421*1.5
+      }
+
+      this.onRegionChange(region, region.latitude, region.longitude);
+    });
+  }
+
+  onRegionChange(region, latitude, longitude) {
+    this.setState({
+      mapInitialised: true,
+      mapRegion: region,
+      latitude: latitude || this.state.latitude,
+      longitude: longitude || this.state.longitude
+    });
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchID);
+  }
 
   render() {
     return (
       <View style={styles.container}>
         <StatusBar hidden={true}/>
         <View style={styles.contentContainer}>
+          {this.state.mapInitialised ? (
+              <MapView
+                style={{ alignSelf: 'stretch', height: 250 }}
+                region={this.state.mapRegion}
+                provider={MapView.PROVIDER_GOOGLE}
+                onRegionChange={this.onRegionChange.bind(this)}
+              >
+                <MapView.Marker
+                  coordinate={{
+                    latitude: (this.state.latitude + 0.00050),
+                    longitude: (this.state.longitude + 0.00050),
+                  }}
+                />
+              </MapView>
+            ) : (
+              <View style={styles.initMap}>
+                <Text>Initialising map...</Text>
+              </View>
+            )
+          }
           <Modal isVisible={this.state.showModal}>
             <View style={styles.modalContent}>
               <Text>Looking up address from current GPS...</Text>
@@ -67,11 +118,17 @@ const styles = StyleSheet.create({
   contentContainer: {
     flex: 1,
   },
+  initMap: {
+    height: 250,
+    backgroundColor: '#ddd',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
   modalContent: {
     backgroundColor: 'white',
     padding: 22,
-    justifyContent: 'center',
     alignItems: 'center',
+    justifyContent: 'center',
     borderRadius: 4,
     borderColor: 'rgba(0, 0, 0, 0.1)',
   },
